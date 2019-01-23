58d9f998d62c8e61a7987bfc7a0f0dfed4fd9f69
Metahaem <tom.rowson@gmail.com>
2019-01-23 11:01:41 +0000

    Toggle category classes: active/inactive

--- src/index.js
+++ src/index.js
@@ -5,21 +5,14 @@ const actDoneCountEl = document.querySelector(".done-count-container")
 //const userDoneCountEl
 const generateButton = document.querySelector(".generate-button")
 const doneButton = document.querySelector(".done-button")
-const allButton = document.querySelector(".all-categories")
-const animalsButton = document.querySelector(".animals")
-const environmentButton = document.querySelector(".environment")
-const familyButton = document.querySelector(".family-friends")
-const charityButton = document.querySelector(".charity")
-const workButton = document.querySelector(".work")
+const allButton = document.querySelector("#all-categories")
+const animalsButton = document.querySelector("#animals")
+const environmentButton = document.querySelector("#environment")
+const familyButton = document.querySelector("#family-friends")
+const charityButton = document.querySelector("#charity")
+const workButton = document.querySelector("#work")
 
-function onCatButton(event) {
-  const catId = parseInt(event.target.dataset.id)
-  state.selectedCategories.add(catId)
-}
-
-function onAllButton(event) {
-  state.selectedCategories = new Set([1,2,3,4,5])
-}
+const arrayOfCategories = [animalsButton, environmentButton, familyButton, charityButton, workButton]
 
 allButton.addEventListener(`click`, onAllButton)
 animalsButton.addEventListener(`click`, onCatButton)
@@ -57,6 +50,34 @@ function init() {
   fetchUsersFromAPI()
 }
 
+//-----------Event Listener Functions----------
+
+function onCatButton(event) {
+  const catId = parseInt(event.target.dataset.id)
+  if (event.target.class === "active") {
+    event.target.class = ""
+    state.selectedCategories.delete(catId)
+  }
+  else {
+    event.target.class = "active"
+    state.selectedCategories.add(catId)
+  }
+}
+
+function onAllButton(event) {
+  if (allButton.class === "active") {
+    allButton.class = ""
+    state.selectedCategories = new Set([])
+    arrayOfCategories.forEach(category => category.class = "")
+  }
+  else {
+    state.selectedCategories = new Set([1,2,3,4,5])
+    allButton.class = "active"
+    arrayOfCategories.forEach(category => category.class = "active")
+  }
+
+}
+
 //----------Render Act data to page--------------------
 
 function renderAct(id) {
@@ -72,17 +93,21 @@ function getRandomIndex(actArray) {
 //returns random index within
 
 function randomActFromSelectedCategoryIDs() {
-  //MOCKED UP. USE COMMENTED LINES WHEN HAVE CAT BUTTONS SET UP
   const actArray = state.acts.filter(act => state.selectedCategories.has(act.category_id))
   const actIndex = getRandomIndex(actArray)
-  // const actIndex = getRandomIndex(state.acts)
   return actArray[actIndex]
 }
 //selects random Act that matches the filtered categories
 
 function onGenerateButton() {
-  const id = randomActFromSelectedCategoryIDs().id
-  renderAct(id)
+  if (state.selectedCategories.size > 0) {
+    const id = randomActFromSelectedCategoryIDs().id
+    renderAct(id)
+  }
+  else {
+    actImageEl.src = ""
+    actTextEl.innerText = "Please select a category."
+  }
 }
 //final function to call when generate button clicked
 
